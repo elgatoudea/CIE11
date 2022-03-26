1d55bed71ea212ee6b57220ff53ed9c35aa720a5
Duván Villa <duvan.villa@vitalbox.co>
2022-03-26 01:43:39 -0500

    Actualización del proyecto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :first_time_visit, unless: -> { session[:first_visit] }
 
-  # Alla prima visita imposto i cookie per effettuare correttamente la richiesta
+  # En la primera visita configuro cookies para realizar la solicitud correctamente
   def first_time_visit
     session[:first_visit] = 1
     session[:expire] = Time.now
--- app/controllers/interaction_controller.rb
+++ app/controllers/interaction_controller.rb
@@ -1,22 +1,22 @@
 class InteractionController < ApplicationController
   def new
 
-    # richiedo il token di autorizzazione solo se necessario
-    # Se il parametro della sessione non è stato inizializzato
-    # O se è passata un ora da quando è stato inizializzato
+    # Solicito el token de autorización solo si es necesario
+     # Si el parámetro de sesión no ha sido inicializado
+     # O si ha pasado una hora desde que se inicializó
     if @first_visit or Time.now >= session[:expire]
-      # Processo di autenticazione
+      # Proceso de autenticación
       response = RestClient.post 'https://icdaccessmanagement.who.int/connect/token',
                                   {
                                       scope: 'icdapi_access',
                                       grant_type: 'client_credentials',
-                                      client_id: 'client id',
-                                      client_secret: 'client secret'
+                                      client_id: '5b967c45-ad6a-4ffd-9d01-69cb1e34c09e_2ce9ad47-3ecb-416c-b95b-6b598f462cd5',
+                                      client_secret: '4mx7I0oCyuZJRJ4oUe4XnTxQw6mUh0wFaLtjSs5eZIM='
                                   }
-      # Il token scade dopo un ora
+      # El token caduca después de una hora
       session[:expire] = Time.now + 1.hour
 
-      # Parsing della risposta
+      # Analizando la respuesta
       @response = JSON.parse(response)
       session[:token] = @response["access_token"]
 
@@ -27,16 +27,16 @@ class InteractionController < ApplicationController
   end
 
   def show
-    # costruisco l'uri con il parametro desiderato
-    # Se il parametro di ricerca non è stato passato ritorno alla pagina precedente
+    # construye el uri con el parámetro deseado
+     # Si no se ha pasado el parámetro de búsqueda, volver a la página anterior
     if params[:ricerca] == ""
-      redirect_to interaction_new_path, notice: 'Inserire un parametro di ricerca!'
+      redirect_to interaction_new_path, notice: 'Introduzca un parámetro de búsqueda!'
     else
       uri = "http://id.who.int/icd/entity/search?q={#{params[:ricerca]}}"
 
       response = RestClient.get uri, {'Authorization': "Bearer #{session[:token]}",
                                       'Accept': :json,
-                                      'Accept-Language': 'en',
+                                      'Accept-Language': 'es',
                                       'API-Version': 'v2'
       }
       @response = JSON.parse(response)
